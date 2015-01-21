3079462113e81d4c3a5956fbcb96475360e8afac
patrickmcwilliams <patrick.mcwilliams@tealium.com>
2015-01-21 10:57:20 -0800

    added no view option to config

--- tealium.js
+++ tealium.js
@@ -1,32 +1,25 @@
  angular.module('TealiumConfigure', [])
   .factory('tealium_configure', ["$location", function($location){
-      var get_config = function() {
+      return function() {
         // import any modules and setup here for view_id and data_connector
         
-        var view_id = $location.path();            // place reference or function to return unique key for current view
-        var data_connector  = {};                  // place reference or function to return data model
-        var ui_selectors    = '.trackable, input'; // elements to be autotracked *this is a sample*
-        var account     = "tealiummobile";         // add tealium account name
-        var profile     = "demo";                  // add profile name to use
-        var environment = "dev";                   // add target environment to use
+        this.view_id = $location.path();            // place reference or function to return unique key for current view
+        this.data_connector  = {};                  // place reference or function to return data model
+        this.ui_selectors    = '.trackable, input'; // elements to be autotracked *this is a sample*
+        this.account     = "tealiummobile";         // add tealium account name
+        this.profile     = "demo";                  // add profile name to use
+        this.environment = "dev";                   // add target environment to use
+        this.suppress_first_view = false;           // set to true if you are calling view for app load yourself
         
 // DO NOT EDIT BELOW THIS LINE ---------------------------------------------------------            
-        return {
-            "uiSelectors": ui_selectors,
-            "view_id": view_id,
-            "data_connector" : data_connector,
-            "account" : account,
-            "profile" : profile,
-            "environment" : environment
-        };
       };
-      return get_config;
     }
   ]);
 
  
  angular.module('TealiumHelper', ['TealiumUDO', 'TealiumConfigure'])
    .factory('tealium', ['tealium_configure', 'tealium_udo', function(tealium_configure, tealium_udo) {
+      var config = new tealium_configure();
       var link = function(udo, e) {
          var b = {};
          angular.forEach(udo, function(value, key) {
@@ -43,13 +36,13 @@
        };
 
       var view = function() {
-         udo = tealium_udo(tealium_configure()) ? tealium_udo(tealium_configure())["data"] : {};
+         udo = tealium_udo(config) ? tealium_udo(config)["data_connector"] : {};
          if (window.utag){
-         utag.view(udo);
-         angular.element(document.querySelectorAll(tealium_configure().uiSelectors))
-           .bind('click', function(e) {
-             link(udo, e);
-           });
+           utag.view(udo);
+           angular.element(document.querySelectorAll(tealium_configure().uiSelectors))
+             .bind('click', function(e) {
+               link(udo, e);
+             });
          }
        };
 
@@ -58,11 +51,15 @@
    }])
    .run(function(tealium_configure) {
      // like main when class is called
+     var config = new tealium_configure();
+     if (config.suppress_first_view){
+       window.utag_cfg_ovrd = {noview : true};
+     }
      (function(a, b, c, d) {
        a = '//tags.tiqcdn.com/utag/'
-        + tealium_configure().account
-        +'/'+ tealium_configure().profile
-        +'/'+ tealium_configure().environment
+        + config.account
+        +'/'+ config.profile
+        +'/'+ config.environment
         +'/utag.js';
        b = document;
        c = 'script';
